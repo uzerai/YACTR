f161ebac74ea0ec0bdf9a1e258cb634ffb918f59
E. Botten <edvbot@gmail.com>
2025-07-05 11:14:00 +0200

    feat(async-data): update repository methods to accept and utilize CancellationTokens

--- src/YACTR/Data/Repository/Interface/IEntityRepository.cs
+++ src/YACTR/Data/Repository/Interface/IEntityRepository.cs
@@ -8,8 +8,9 @@ public interface IEntityRepository<T> : IRepository<T> where T : BaseEntity
     /// Get an entity by its ID without tracking changes.
     /// </summary>
     /// <param name="id">The unique identifier of the entity.</param>
+    /// <param name="ct">Cancellation token</param>
     /// <returns>The entity if found; otherwise, null.</returns>
-    Task<T?> GetByIdAsync(Guid id);
+    Task<T?> GetByIdAsync(Guid id, CancellationToken ct = default);
 
     /// <summary>
     /// Get an entity by its ID with change tracking enabled.
@@ -17,6 +18,7 @@ public interface IEntityRepository<T> : IRepository<T> where T : BaseEntity
     /// Aims to be the only entrypoint to getting a 
     /// </summary>
     /// <param name="id">The unique identifier of the entity.</param>
+    /// <param name="ct">Cancellation token</param>
     /// <returns>The entity if found; otherwise, null.</returns>
-    Task<T?> GetByIdTrackingAsync(Guid id);
+    Task<T?> GetByIdTrackingAsync(Guid id, CancellationToken ct = default);
 }
