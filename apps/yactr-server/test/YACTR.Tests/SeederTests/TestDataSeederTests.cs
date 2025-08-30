95c750ded1402fafd735870fefd98f054d7a5b16
E. Botten <edvbot@gmail.com>
2025-08-30 17:20:47 +0200

    fix(test-concurrency): no test concurrency is possible in current setup, since we use a single DB for all tests

--- apps/yactr-server/test/YACTR.Tests/SeederTests/TestDataSeederTests.cs
+++
@@ -1,8 +0,0 @@
-using FastEndpoints.Testing;
-
-namespace YACTR.Tests.SeederTests;
-
-[Collection("SeederTests")]
-public class TestDataSeedertests(IntegrationTestClassFixture fixture) : TestBase<IntegrationTestClassFixture>
-{
-}
