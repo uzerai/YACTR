d4eeeef7c7b16dbf50327ca3860fd2d9bb55f0a1
E. Botten <edvbot@gmail.com>
2025-09-28 09:06:16 +0200

    feat(pitches): add order and svg columns to pitch

--- src/YACTR/Data/Migrations/20250927191222_AddOrderAndSVGInformationToPitch.Designer.cs
+++ src/YACTR/Data/Migrations/20250927191222_AddOrderAndSVGInformationToPitch.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using NodaTime;
@@ -16,9 +17,11 @@ using YACTR.Data.Model.Climbing;
 namespace YACTR.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250927191222_AddOrderAndSVGInformationToPitch")]
+    partial class AddOrderAndSVGInformationToPitch
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
