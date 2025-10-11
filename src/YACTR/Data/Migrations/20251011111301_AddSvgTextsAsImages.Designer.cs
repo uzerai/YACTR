39194071729da6fa673257d9f4b9a60d723785fc
E. Botten <edvbot@gmail.com>
2025-10-11 13:39:52 +0200

    feat(svg-overlay): add columns for svg overlay images

--- src/YACTR/Data/Migrations/20251011111301_AddSvgTextsAsImages.Designer.cs
+++ src/YACTR/Data/Migrations/20251011111301_AddSvgTextsAsImages.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using NodaTime;
@@ -16,9 +17,11 @@ using YACTR.Data.Model.Climbing;
 namespace YACTR.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20251011111301_AddSvgTextsAsImages")]
+    partial class AddSvgTextsAsImages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
