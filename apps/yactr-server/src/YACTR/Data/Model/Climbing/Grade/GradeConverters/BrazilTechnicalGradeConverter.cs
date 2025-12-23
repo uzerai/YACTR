d7d167544b9c023c9ee6b971bad18f3acee6702c
E. Botten <edvbot@gmail.com>
2025-12-23 20:40:30 +0100

    chore(grade-converters-namespace): update namespace for grade converters

--- apps/yactr-server/src/YACTR/Data/Model/Climbing/Grade/GradeConverters/BrazilTechnicalGradeConverter.cs
+++ apps/yactr-server/src/YACTR/Data/Model/Climbing/Grade/GradeConverters/BrazilTechnicalGradeConverter.cs
@@ -1,4 +1,4 @@
-namespace YACTR.Data.Model.Climbing.Grade;
+namespace YACTR.Data.Model.Climbing.Grade.Converter;
 
 public class BrazilTechnicalGradeConverter : GradeConverter
 {
@@ -6,11 +6,11 @@ public class BrazilTechnicalGradeConverter : GradeConverter
     {
         GradeSystem = GradeSystemEnum.BrazilTechnical;
         GradeRanges = [
-            (2, 19, "I"), (20, 37, "Isup"), (38, 55, "II"), (56, 74, "IIsup"), (75, 92, "III"), (93, 115, "IIIsup"),
+            (0, 19, "I"), (20, 37, "Isup"), (38, 55, "II"), (56, 74, "IIsup"), (75, 92, "III"), (93, 115, "IIIsup"),
             (116, 137, "IV"), (138, 160, "IVsup"), (161, 183, "V"), (184, 195, "Vsup"), (196, 209, "VI"), (210, 222, "VIsup"),
             (223, 235, "VIIa"), (236, 248, "VIIb"), (249, 261, "VIIc"), (262, 274, "VIIIa"), (275, 285, "VIIIb"), (286, 296, "VIIIc"),
             (297, 308, "IXa"), (309, 319, "IXb"), (320, 331, "IXc"), (332, 342, "Xa"), (343, 354, "Xb"), (355, 365, "Xc"),
-            (366, 377, "XIa"), (378, 391, "XIb"), (392, 404, "XIc"), (405, 416, "XIIa"), (417, 430, "XIIb"), (431, 443, "XIIc")
+            (366, 377, "XIa"), (378, 391, "XIb"), (392, 404, "XIc"), (405, 416, "XIIa"), (417, 430, "XIIb"), (431, 500, "XIIc")
         ];
     }
 }
--- apps/yactr-server/src/YACTR/Data/Model/Climbing/Grade/GradeConverters/BritishTechnicalGradeConverter.cs
+++ apps/yactr-server/src/YACTR/Data/Model/Climbing/Grade/GradeConverters/BritishTechnicalGradeConverter.cs
@@ -1,4 +1,4 @@
-namespace YACTR.Data.Model.Climbing.Grade;
+namespace YACTR.Data.Model.Climbing.Grade.Converter;
 
 public class BritishTechnicalGradeConverter : GradeConverter
 {
@@ -6,10 +6,10 @@ public class BritishTechnicalGradeConverter : GradeConverter
     {
         GradeSystem = GradeSystemEnum.BritishTechnical;
         GradeRanges = [
-            (2, 10, "1a"), (11, 19, "1b"), (20, 28, "1c"), (29, 37, "2a"), (38, 46, "2b"), (47, 55, "2c"),
+            (0, 10, "1a"), (11, 19, "1b"), (20, 28, "1c"), (29, 37, "2a"), (38, 46, "2b"), (47, 55, "2c"),
             (56, 65, "3a"), (66, 74, "3b"), (75, 83, "3c"), (84, 92, "4a"), (93, 122, "4b"), (123, 153, "4c"),
             (154, 183, "5a"), (184, 213, "5b"), (214, 244, "5c"), (245, 274, "6a"), (275, 304, "6b"), (305, 335, "6c"),
-            (336, 365, "7a"), (366, 395, "7b"), (396, 425, "7c")
+            (336, 365, "7a"), (366, 395, "7b"), (396, 500, "7c")
         ];
     }
 }
