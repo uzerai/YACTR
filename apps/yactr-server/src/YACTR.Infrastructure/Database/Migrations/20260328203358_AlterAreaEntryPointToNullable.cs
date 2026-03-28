b051ef8432c5333e257e4d1fd4f9c84989d5ee2b
E. Botten <edvbot@gmail.com>
2026-03-28 21:35:01 +0100

    fix(area): make entry-point nullable as requiring it was a chore

---
+++ apps/yactr-server/src/YACTR.Infrastructure/Database/Migrations/20260328203358_AlterAreaEntryPointToNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace YACTR.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterAreaEntryPointToNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Point>(
+                name: "entry_point",
+                table: "sectors",
+                type: "geometry",
+                nullable: true,
+                oldClrType: typeof(Point),
+                oldType: "geometry");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Point>(
+                name: "entry_point",
+                table: "sectors",
+                type: "geometry",
+                nullable: false,
+                oldClrType: typeof(Point),
+                oldType: "geometry",
+                oldNullable: true);
+        }
+    }
+}
