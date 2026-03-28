88c7fbdda1c713187dd1c8c5a2398dd510ab9026
E. Botten <edvbot@gmail.com>
2026-03-28 12:39:57 +0100

    feat(simple-filters): simplify filters, avoid complexity

--- src/YACTR.Api/Endpoints/Areas/GetAllAreas.cs
+++ src/YACTR.Api/Endpoints/Areas/GetAllAreas.cs
@@ -1,12 +1,28 @@
 using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
+using NodaTime;
 using YACTR.Api.Pagination;
 using YACTR.Domain.Model.Climbing;
 using YACTR.Infrastructure.Database.Repository.Interface;
 
 namespace YACTR.Api.Endpoints.Areas;
 
-public class GetAllAreasRequest : PaginationRequest {};
+public class GetAllAreasRequest : PaginationRequest {
+    /// <summary>
+    /// Contains-matched name of the area. ie: "lo" will match "london" and "london wall"
+    /// </summary>
+    public string? Name { get; init; }
+
+    /// <summary>
+    /// Areas created before the given instant.
+    /// </summary>
+    public Instant? CreatedBefore { get;init; }
+
+    /// <summary>
+    /// Areas created after the given instant.
+    /// </summary>
+    public Instant? CreatedAfter { get;init; }
+};
 
 public class GetAllAreas : Endpoint<GetAllAreasRequest, PaginatedResponse<AreaResponse>, AreaDataMapper>
 {
@@ -21,11 +37,34 @@ public class GetAllAreas : Endpoint<GetAllAreasRequest, PaginatedResponse<AreaRe
 
     public override async Task HandleAsync(GetAllAreasRequest req, CancellationToken ct)
     {
-        var areas = await AreaRepository.AllAvailable()
-            .AsNoTracking()
-            .OrderBy(e => e.Id)
+        var query = AreaRepository.AllAvailable()
+            .AsNoTracking();
+
+        query = ApplyFilters(query, req);
+
+        var result = await query.OrderBy(e => e.Id)
             .ToPaginatedResponseAsync(Map.FromEntityAsync, req, ct);
             
-        await Send.OkAsync(areas, cancellation: ct);
+        await Send.OkAsync(result, cancellation: ct);
+    }
+
+    private static IQueryable<Area> ApplyFilters(IQueryable<Area> query, GetAllAreasRequest req)
+    {
+        if (req.Name is not null)
+        {
+            query = query.Where(e => e.Name.Contains(req.Name));
+        }
+
+        if (req.CreatedBefore is not null)
+        {
+            query = query.Where(e => e.CreatedAt < req.CreatedBefore);
+        }
+
+        if (req.CreatedAfter is not null)
+        {
+            query = query.Where(e => e.CreatedAt > req.CreatedAfter);
+        }
+
+        return query;
     }
 }
